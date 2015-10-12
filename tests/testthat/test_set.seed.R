cb9ad062632d838824a94d2714cb2f8fe7a820e1
robingenuer <robin.genuer@isped.u-bordeaux2.fr>
2015-10-12 09:58:47 +0200

    maj test rf

--- tests/testthat/test_set.seed.R
+++ tests/testthat/test_set.seed.R
@@ -1,24 +1,19 @@
 context("randomness in runif and randomForest")
 
+platform <- sessionInfo()$platform
+is.64 <- function(platform) {
+  if (substring(platform, nchar(platform) - 6, nchar(platform) - 1) == "64-bit") {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
 set.seed(2219)
-def <- runif(1)
-
-test_that("Different kind in set.seed", {
-  expect_equal(def, 0.4305179, tolerance = 1e-7)
-})
-
 data(iris)
 iris.rf <- randomForest(iris[,1:4], iris[,5])
 
-platform <- sessionInfo()$platform
-if (substring(platform, nchar(platform)-6, nchar(platform)-1) == "64-bit") {
-  test_that("OOB error of rf", {
-    expect_equal(iris.rf$err.rate[500], 0.04666667, tolerance = 1e-7)
-  })
-}
-
-else {
-  test_that("OOB error of rf", {
-    expect_equal(iris.rf$err.rate[500], , tolerance = 1e-7)
-  })
-}
+test_that("OOB error of rf", {
+  expect_equal(iris.rf$err.rate[500],
+               ifelse(is.64(platform), 0.04666667, 0.04), tolerance = 1e-7)
+})
