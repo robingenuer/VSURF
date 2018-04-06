5aea87e04892c3b3b352ef0273600428846db3f2
Robin Genuer <Robin.Genuer@u-bordeaux.fr>
2018-04-06 17:32:15 +0200

    add PM10 data

---
+++ R/PM10.R
@@ -0,0 +1,23 @@
+##' TITLE
+##'
+##' DESCRIPTION
+##' 
+##' @format The format is a list of 2 component:
+##' 
+##' $x: A data-frame containing input variables: with 100 obs. of 200
+##' variables ;
+##' 
+##' $y: Output variable: a factor with 2 levels "-1" and "1".
+##' 
+##' @examples
+##' 
+##' ##'
+##' \dontrun{
+##' 
+##' }
+##' 
+##' @source   Weston, J., Elisseff, A., Schoelkopf, B., Tipping, M. (2003),
+##' \emph{Use of the zero norm with linear models and Kernel methods},
+##' J. Machine Learn. Res. 3, 1439-1461
+##'
+"jus"
