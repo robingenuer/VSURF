43a346cb4c6f02922a933d96865ab332d94ace40
Mélanie HUCHON <huchon.melanie@gmail.com>
2021-05-18 15:40:31 +0200

    add survival part in VSURF_pred

--- R/VSURF_pred.R
+++ R/VSURF_pred.R
@@ -314,8 +314,144 @@ did not eliminate variables")
 
 #' @rdname VSURF_pred
 #' @export
-VSURF_pred.formula <- function(formula, data, ..., na.action = na.fail) {
+VSURF_pred.formula <- function(formula, data, ntree = 2000, err.interp, varselect.interp,
+                               nfor.pred = 25, nmj = 1, RFimplem = "randomForest", parallel = FALSE,
+                               ncores = detectCores()-1, verbose = TRUE,  importance="permute", block.size = 1,..., na.action = na.fail) {
 ### formula interface for VSURF_pred.
+  
+  ### code gratefully stolen from rfsrc (package randomForestSRC) and adapted for this function.
+  
+  ## conduct preliminary formula validation
+  formulaPrelim <- parseFormula(formula, data)
+  ## save the call/formula for the return object
+  my.call <- match.call()
+  my.call$formula <- eval(formula)
+  
+  ## finalize the formula based on the pre-processed data
+  formulaDetail <- finalizeFormula(formulaPrelim, data)  
+  
+  ## save the family for convenient access
+  family <- formulaDetail$family
+  
+  if (family == "surv"){
+    RFimplem <- "randomForestSRC"
+    
+    # err.interp: interpretation models errors
+    # varselect.interp: interpretation variables indices
+    # nmj: number of mean jump: addition of a variable if it gives an error
+    # reduction of at least nmj * mean jump value
+    
+    start <- Sys.time()
+    
+    #num of the variables used for Surv in data
+    num_surv <- which(colnames(data) == formulaDetail$yvar.names[1] | colnames(data)==formulaDetail$yvar.names[2])
+   
+     if (verbose == TRUE) cat(paste("\nPrediction step (on", length(varselect.interp),
+                                   "variables)\n"))
+    
+    
+    if (verbose == TRUE) {
+      if (RFimplem=="randomForestSRC"){
+        timeOneRFAllVar <- system.time(
+          randomForestSRC::rfsrc(formula, data=data[,c(varselect.interp, num_surv), drop=FALSE],
+                                 ...)
+        )
+      }
+      cat(paste("Maximum estimated computational time (on one core):",
+                round(length(varselect.interp) * nfor.pred * timeOneRFAllVar[3], 1), "sec.\n"))
+    }
+    
+    # initialization of the progress bar
+    if (verbose == TRUE) {
+      pb <- utils::txtProgressBar(style = 3)
+      nBar <- 1
+    }
+    
+    k <- length(err.interp)
+    l <- length(varselect.interp)
+    
+    if (k==l) {
+      warning(
+        "Unable to perform prediction step, because the interpretation step
+did not eliminate variables")
+      varselect.pred <- NULL
+      err.pred <- NULL
+      mean.jump <- NULL
+    } else {
+      # mean jump calculation
+      s=NULL
+      for (i in l:(k-1)){
+        s <- c(s, abs(err.interp[i+1] - err.interp[i]) )
+      }
+      mean.jump <- mean(s)
+      
+      # comparison between the error with the variable and the precedent error
+      # and test of the addition of the variable
+      n <- nrow(data)
+      varselect.pred <- varselect.interp[1]
+      u <- c(varselect.pred, num_surv)
+      w <- data[, u, drop=FALSE]
+      rf <- rep(NA, nfor.pred)
+      
+      if (RFimplem == "randomForestSRC"){
+        for (j in 1:nfor.pred) {
+          rf[j]<-randomForestSRC::rfsrc(formula, data=w, ntree=ntree, importance=importance, block.size = block.size, ...)$err.rate[ntree]
+        }
+        err.pred <- mean(rf)
+      }
+      
+      t <- err.pred
+      
+      if (verbose == TRUE) {
+        utils::setTxtProgressBar(pb, nBar/l)
+        nBar <- nBar + 1
+      }
+      
+      if (l>1) {
+        for (i in 2:l){
+          u <- c(varselect.pred, varselect.interp[i], num_surv)
+          w <- data[, u, drop=FALSE]
+          rf <- rep(NA, nfor.pred)
+          
+          if (RFimplem == "randomForestSRC"){
+            for (j in 1:nfor.pred) {
+              rf[j]<-randomForestSRC::rfsrc(formula, data=w, ntree=ntree, importance=importance, block.size = block.size, ...)$err.rate[ntree]
+            }
+            z <- mean(rf)
+          }
+          
+          if ((t-z) > nmj*mean.jump){
+            varselect.pred <- c(varselect.pred, varselect.interp[i])
+            err.pred <- c(err.pred, z)
+            t <- z
+          }
+          if (verbose == TRUE) {
+            utils::setTxtProgressBar(pb, nBar/l)
+            nBar <- nBar + 1
+          }
+        }
+      }
+    }
+    
+    cl <- match.call()
+    cl[[1]] <- as.name("VSURF_pred")
+    
+    comput.time <- Sys.time()-start
+    
+    output <- list('varselect.pred'=varselect.pred,
+                   'err.pred'=err.pred,
+                   'mean.jump'=mean.jump,
+                   'num.varselect.pred'=length(varselect.pred),
+                   'nmj' = nmj,
+                   'comput.time'=comput.time,
+                   'RFimplem' = RFimplem,
+                   'call'=cl)
+    class(output) <- c("VSURF_pred")
+    output
+  }
+  
+  else{
+  
 ### code gratefully stolen from svm.formula (package e1071).
 ###
     if (!inherits(formula, "formula"))
@@ -356,4 +492,105 @@ VSURF_pred.formula <- function(formula, data, ..., na.action = na.fail) {
   which are indices of the input matrix based on the formula:
   you may reorder these to get indices of the original data")
     return(ret)
+  }
+}
+
+
+#_______________________________________________
+#functions from randomForestSRC
+parseFormula <- function(f, data, ytry = NULL, coerce.factor = NULL) {
+  ## confirm coherency of the formula
+  if (!inherits(f, "formula")) {
+    stop("'formula' is not a formula object.")
+  }
+  if (is.null(data)) {
+    stop("'data' is missing.")
+  }
+  if (!is.data.frame(data)) {
+    stop("'data' must be a data frame.")
+  }
+  ## pull the family and y-variable names
+  fmly <- all.names(f, max.names = 1e7)[2]
+  all.names <- all.vars(f, max.names = 1e7)
+  yvar.names <- all.vars(formula(paste(as.character(f)[2], "~ .")), max.names = 1e7)
+  yvar.names <- yvar.names[-length(yvar.names)]
+  ## Default scenario, no subject information when family is not
+  ## time dependent covariates.  Can be overridden later.
+  subj.names <- NULL
+  ## is coerce.factor at play for the y-outcomes?
+  coerce.factor.org <- coerce.factor
+  coerce.factor <- vector("list", 2)
+  names(coerce.factor) <- c("xvar.names", "yvar.names")
+  if (!is.null(coerce.factor.org)) {
+    coerce.factor$yvar.names <- intersect(yvar.names, coerce.factor.org)
+    if (length(coerce.factor$yvar.names) == 0) {
+      coerce.factor$yvar.names <- NULL
+    }
+    coerce.factor$xvar.names <- intersect(setdiff(colnames(data), yvar.names), coerce.factor.org)
+  }
+  ## survival forests
+  if (fmly == "Surv") {
+    ## Survival and competing risk will have 2 slots, namely time and censoring.
+    ## Time dependent covariates will have 4 slots, namely id, start, stop, and event.
+    ## If TDC is in effect, we remove the id from the yvars, and tag is an the subject identifier.
+    if ((sum(is.element(yvar.names, names(data))) != 2) &&
+        (sum(is.element(yvar.names, names(data))) != 4)) {
+      stop("Survival formula incorrectly specified.")
+    }
+    else {
+      if (sum(is.element(yvar.names, names(data))) == 4) {
+        ## Time dependent covariates is in effect.
+        subj.names <- yvar.names[1]
+        yvar.names <- yvar.names[-1]
+      }
+    }
+    family <- "surv"
+    ytry <- 0
+  }
+  ## done: return the goodies
+  return (list(all.names=all.names, family=family, subj.names=subj.names, yvar.names=yvar.names, ytry=ytry,
+               coerce.factor = coerce.factor))
+}
+
+finalizeFormula <- function(formula.obj, data) {
+  ## parse the formula object
+  yvar.names <- formula.obj$yvar.names
+  subj.names <- formula.obj$subj.names
+  all.names  <- formula.obj$all.names
+  fmly       <- formula.obj$family
+  ytry       <- formula.obj$ytry
+  index <- length(yvar.names)
+  ## Adjust the index for the presence of subject names.
+  if (fmly == "surv") {
+    if (!is.null(subj.names)) {
+      index <- index + 1
+    }
+  }
+  ## total number of variables should exceed number of yvars
+  if (length(all.names) <= index) {
+    stop("formula is misspecified: total number of variables does not exceed total number of y-variables")
+  }
+  ## extract the xvar names
+  if (all.names[index + 1] == ".") {
+    if(index == 0) {
+      xvar.names <- names(data)
+    }
+    else {
+      xvar.names <- names(data)[!is.element(names(data), all.names[1:index])]
+    }
+  }
+  else {
+    if(index == 0) {
+      xvar.names <- all.names
+    }
+    else {
+      xvar.names <- all.names[-c(1:index)]
+    }
+    not.specified <- !is.element(xvar.names, names(data))
+    if (sum(not.specified) > 0) {
+      stop("formula is misspecified, object ", xvar.names[not.specified], " not found")
+    }
+  }
+  ## return the goodies
+  return (list(family=fmly, subj.names=subj.names, yvar.names=yvar.names, xvar.names=xvar.names, ytry=ytry))
 }
